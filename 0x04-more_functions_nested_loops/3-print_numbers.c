c541625d70865f945f8f6651fbd6706a3ba469e7
Emmanuel <akedlepp@gmail.com>
2022-10-21 20:39:17 -0700

    function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print num from 0 - 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char a = 0;
+
+	while (a < 10)
+		_putchar(a);
+	{
+		_putchar(a++ + '0');
+	}
+
+	_putchar('\n');
+}
