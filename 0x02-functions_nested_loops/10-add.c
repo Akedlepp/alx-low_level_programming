f94f47c936770a230a7a563835748de9f4925b3d
Emmanuel <akedlepp@gmail.com>
2022-10-23 17:00:20 -0700

    0x02function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.c"
+
+/**
+ * _intadd - add two integers and return the results
+ * @a: integer to be added
+ * @b: integer to be added
+ * Return: addition of both int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
